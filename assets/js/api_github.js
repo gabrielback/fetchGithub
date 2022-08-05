cc2ced7e2abab5cadffd4386a36ed7419de6967e
Gabriel-Gramaq <gabrielluiz-back@hotmail.com>
2022-08-05 13:11:52 -0300

    Update mixed content

--- assets/js/api_github.js
+++ assets/js/api_github.js
@@ -25,76 +25,87 @@ const getUserRepositories = (user) => {
     .then(
         getJson('../json/content.json')
         .then(data => {
-            renderizarDashboard(data)
+            renderDashboard(data)
         })
         .then(
             getJson(`https://api.github.com/users/${githubUserName}`)
             .then(data => {
-                renderizarUsuario(data)
+                renderUser(data)
+                
             })
-        )
-    )
-}
-getUserRepositories(githubUserName)
-
-
-
-const renderizarRepositorios = repositories => {
-    const conteudoGithub = document.querySelector('.conteudoGithub');
-    conteudoGithub.insertAdjacentHTML("afterbegin", `<h1>My Git Repositories</h1><div id="lista-repositorios">`)
-    let listaDeRepositorios = conteudoGithub.firstChild.nextSibling
-    repositories.map(repository => {
-        let privacy = repository.private ? "private" : "public"
-        let updatedAt = Intl.DateTimeFormat('pt-br').format(new Date(repository.updated_at))
-        listaDeRepositorios.innerHTML += 
-        ` 
-        <div onclick="loadGithub('${repository.html_url}')" class="repositorio ${privacy}">
-        <h1>${repository.name}</h1>
-        <p>${privacy}</p>
-        <div>
-        <p class="language">${repository.language ? repository.language : ""}<p>
-        <p class="date">Last update: ${updatedAt}</p>
-        <div>
-        <div>
-        `
-    })
-    conteudoGithub.innerHTML += "</div>"
-}
-
-function renderizarDashboard(data){
-    let dashboard = document.getElementById('dashboard')
-    dashboard.insertAdjacentHTML("afterbegin", `
-    <h1>${data[0].title}</h1>
-        <div>
+            )
+            )
+        }
+        getUserRepositories(githubUserName)
+        
+        
+        
+        const renderizarRepositorios = repositories => {
+            const conteudoGithub = document.querySelector('.conteudoGithub');
+            conteudoGithub.insertAdjacentHTML("afterbegin", `<h1>My Git Repositories</h1><div id="lista-repositorios">`)
+            let listaDeRepositorios = conteudoGithub.firstChild.nextSibling
+            repositories.map(repository => {
+                let privacy = repository.private ? "private" : "public"
+                let updatedAt = Intl.DateTimeFormat('pt-br').format(new Date(repository.updated_at))
+                listaDeRepositorios.innerHTML += 
+                ` 
+                <div onclick="loadPage('${repository.html_url}')" class="repositorio ${privacy}">
+                <h1>${repository.name}</h1>
+                <p>${privacy}</p>
+                <div>
+                <p class="language">${repository.language ? repository.language : ""}<p>
+                <p class="date">Last update: ${updatedAt}</p>
+                <div>
+                <div>
+                `
+            })
+            conteudoGithub.innerHTML += "</div>"
+        }
+        
+        function renderDashboard(data){
+            let dashboard = document.getElementById('dashboard')
+            dashboard.insertAdjacentHTML("afterbegin", `
+            <h1>${data[0].title}</h1>
+            <div>
             <iframe id="pbi" width="100%" height="100%" src="" frameborder="0" allowFullScreen="true"></iframe>
-        <div>`)
-    let powerBI = document.getElementById('pbi')
-    powerBI.src = data[0].url
-}
-
-const followers = (url) => {
-    getJson(url)
-    .then(data => data.map(user => {
-        return user
-    }))
-}
-
-followers("https://api.github.com/users/gabrielback/followers")
-
-const renderizarUsuario = data => {
-        document.querySelector('.content').insertAdjacentHTML(
-            "afterbegin",
-            `
-            <div class="header">
-                <div style="display: flex;">
-                    <img id="foto" src="${data.avatar_url}">
-                    <div id="user-name"></div>
-                    <div id="user-info">
-                    <div onclick="loadPage('${data.followers_url}')">Followers: ${data.followers}</div>
-                    <div onclick="loadPage('${data.following_url}')">Following: ${data.following}</div>
+            <div>`)
+            let powerBI = document.getElementById('pbi')
+            powerBI.src = data[0].url
+        }
+        
+        function loadFollowers(url){
+            let htmlAppend =""
+            getJson(url)
+            .then(data => data.map(user => {
+                htmlAppend +=
+                `
+                <div>
+                    <div>${user.login}</div>
+                    <img src="${user.avatar_url}">
                 </div>
-            </div>
-            `
+                `
+                // console.log(htmlAppend)
+                return htmlAppend
+            }))
+        }
+
+        
+        
+        const renderUser = data => {
+            document.querySelector('.content').insertAdjacentHTML(
+                "afterbegin",
+                `
+                <div class="header">
+                <div style="display: flex;">
+                <img id="foto" src="${data.avatar_url}">
+                <div id="user-name"></div>
+                <div id="user-info">
+                <div onclick="loadPage('${data.followers_url}')">Followers: ${data.followers}</div>
+                <div onclick="loadPage('${data.following_url}')">Following: ${data.following}</div>
+                </div>
+                ${loadFollowers("https://api.github.com/users/gabrielback/followers")}
+                </div>
+            </div>`
             )
             document.getElementById('user-name').innerHTML =
             `
